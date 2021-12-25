ef895823642459bfe883515d26c12111e8051db9
fstg <ciditworld@gmail.com>
2021-12-25 15:54:30 -0500

    started 2015/day7 and started 2021/day1

---
+++ 2015/day7/solution.py
@@ -0,0 +1,22 @@
+
+class WirePlan():
+    sources: list[str]
+    action: str
+    output: str
+
+
+def part1():
+    print("\n~~~ Part 1 ~~~\n")
+    with open("input.data", "r") as input_file:
+        pass
+
+
+def part2():
+    print("\n~~~ Part 2 ~~~\n")
+    with open("input.data", "r") as input_file:
+        pass
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
---
+++ 2021/day1/solution.py
@@ -0,0 +1,15 @@
+def part1():
+    print("\n~~~ Part 1 ~~~\n")
+    with open("input.data", "r") as input_file:
+        pass
+
+
+def part2():
+    print("\n~~~ Part 2 ~~~\n")
+    with open("input.data", "r") as input_file:
+        pass
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
