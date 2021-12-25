b9797d552d3b2eac103ae22719931f211e61c22c
fstg <ciditworld@gmail.com>
2021-12-25 15:07:35 -0500

    not the pretiest, but i completed part 2 of day 6

--- 2015/day6/solution.py
+++ 2015/day6/solution.py
@@ -36,25 +36,34 @@ def parse_instruction(unparsed: str):
     instruction.end = parse_coordinate(end)
     return instruction
 
-def make_matrix(n, m):
-    matrix = [False] * n 
+def make_int_matrix(n, m):
+    matrix = [0] * n 
     for x in range(n):
-        matrix[x] = [False] * m
+        matrix[x] = [0] * m
     return matrix
 
-def apply_instruction(lights_grid: list[list[bool]], instruction: Instruction):
+def apply_instruction(lights_grid: list[list[int]], instruction: Instruction, brightness_control: bool = False):
     (x1, y1) = instruction.start
     (x2, y2) = instruction.end
     for x in range(x1, x2+1):
         for y in range(y1, y2+1):
             if instruction.action == Action.TOGGLE:
-                lights_grid[x][y] = not lights_grid[x][y]
+                if brightness_control:
+                    lights_grid[x][y] += 2
+                else:
+                    lights_grid[x][y] = 1 if lights_grid[x][y] == 0 else 0
             elif instruction.action == Action.TURN_ON:
-                lights_grid[x][y] = True
+                if brightness_control:
+                    lights_grid[x][y] += 1
+                else: 
+                    lights_grid[x][y] = 1
             elif instruction.action == Action.TURN_OFF:
-                lights_grid[x][y] = False
+                if brightness_control:
+                    lights_grid[x][y] = lights_grid[x][y] - 1 if lights_grid[x][y] > 0 else 0
+                else:
+                    lights_grid[x][y] = 0
 
-def count_lights_on(lights_grid: list[list[bool]]):
+def count_lights_on(lights_grid: list[list[int]]):
     flatened_lights_grid = [light for lights in lights_grid for light in lights]
     counter = 0
     for light in flatened_lights_grid:
@@ -62,15 +71,21 @@ def count_lights_on(lights_grid: list[list[bool]]):
             counter += 1
     return counter
 
+def count_brightness(lights_grid: list[list[int]]):
+    flatened_lights_grid = [light for lights in lights_grid for light in lights]
+    total_brightness = 0
+    for brightnesses in flatened_lights_grid:
+        total_brightness += brightnesses
+    return total_brightness
 
 
 def part1():
     print("\n~~~ Part 1 ~~~\n")
     with open("input.data", "r") as input_file:
-        matrix = make_matrix(1000, 1000)
+        matrix = make_int_matrix(1000, 1000)
         for line in input_file.readlines():
             instruction = parse_instruction(line)
-            apply_instruction(matrix, instruction)
+            apply_instruction(matrix, instruction, brightness_control=False)
         print(f"the number of lights on is {count_lights_on(matrix)}")
             
 
@@ -78,7 +93,11 @@ def part1():
 def part2():
     print("\n~~~ Part 2 ~~~\n")
     with open("input.data", "r") as input_file:
-        pass
+        matrix = make_int_matrix(1000, 1000)
+        for line in input_file.readlines():
+            instruction = parse_instruction(line)
+            apply_instruction(matrix, instruction, brightness_control=True)
+        print(f"the number of lights on is {count_brightness(matrix)}")
 
 
 if __name__ == "__main__":
