40f5a1833e6fb0dea7e2ee1ba61534a0dc1d45cd
fstg <ciditworld@gmail.com>
2021-12-25 23:12:59 -0500

    made progress on 2021/day3 part 2 but not finished

--- 2021/day3/solution.py
+++ 2021/day3/solution.py
@@ -24,21 +24,36 @@ def calc_gamma_epsilon(sample: list[str]):
     return (int(gamma_string, base=2), int(epsilon_string, base=2))
 
 
+def o2_rating(sample: list[str]):
+    shortest_length = min(map(lambda it: len(it), sample))
+    numbers = list(sample)  # copy
+    while len(numbers) != 1:
+        for index in range(shortest_length):
+            most_common = find_most_common(map(lambda it: it[index], numbers))
+            take = "1" if most_common[0] == most_common[1] else most_common[0]
+            numbers = [
+                number if number[index] == take else take for number in numbers
+            ]
+    return numbers[0]
+
+
 def calc_o2_co2(sample: list[str]):
-    pass    
+    return (o2_rating(sample))
 
 
 def part1():
     print("\n~~~ Part 1 ~~~\n")
     with open("input.data", "r") as input_file:
         (gamma, epsilon) = calc_gamma_epsilon(input_file.readlines())
-        print(f"gamma is {gamma}, epsilon is {epsilon} and consumption rate is {gamma * epsilon}")
+        print(
+            f"gamma is {gamma}, epsilon is {epsilon} and consumption rate is {gamma * epsilon}"
+        )
 
 
 def part2():
     print("\n~~~ Part 2 ~~~\n")
     with open("input.data", "r") as input_file:
-        pass
+        print(calc_o2_co2(input_file.readlines()))
 
 
 if __name__ == "__main__":
