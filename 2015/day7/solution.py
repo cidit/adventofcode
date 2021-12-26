a22b763c023a7243d6af0c627d2402fdb7383e82
fstg <ciditworld@gmail.com>
2021-12-25 23:51:05 -0500

    more brainstorming for 2015/day7

--- 2015/day7/solution.py
+++ 2015/day7/solution.py
@@ -1,11 +1,34 @@
-class UnlinkedNode():
-    def __init__(self, name: str, action: str, dependents: list[str]) -> None:
-        self.dependents = dependents
+
+
+
+"""
+map = {key: hash(name), node: {name: str, action:str, deps: list[str]}}
+"""
+
+class Node:
+    def __init__(self, name: str, action: str, deps: list[str]) -> None:
+        self.deps = deps
         self.action = action
         self.name = name
 
 
-def parse_node(unparsed: str) -> UnlinkedNode:
+def parse_node(unparsed: str) -> Node:
+    """
+    dependents need to be resolved
+    dependents can resolve to a scalar or another dependent
+    there can be at most 2 dependents
+    scalars can only be numbers
+    if a dependent is a numeric string then it is a scalar
+    else, if it is alphabetic it resolves to another dependent
+
+    there are these operations:
+    - RSHIFT: dependent [ACTION] scalar -> output
+    - LSHIFT: dependent [ACTION] scalar -> output
+    - AND: dependent [ACTION] dependent -> output
+    - OR: dependent [ACTION] dependent -> output
+    - NOT: [ACTION] dependent -> output
+    - [NO ACTION]: dependent -> output
+    """
     pass
 
 
