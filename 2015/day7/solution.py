7a28be419851aaa0f4ca09aef49aaf21bd08a777
fstg <ciditworld@gmail.com>
2021-12-27 22:54:19 -0500

    solved 2015/day7 part1 and part2

--- 2015/day7/solution.py
+++ 2015/day7/solution.py
@@ -3,6 +3,7 @@ map = {key: hash(name), node: {name: str, action:str, deps: list[str]}}
 """
 from pathlib import Path
 from typing import NamedTuple
+from logging import log
 
 
 class Node(NamedTuple):
@@ -35,12 +36,11 @@ def parse_node(unparsed: str):
     """
 
     # TODO: refactor this maybe
-    tokens = unparsed.split()
     acions_with_two_dependents = "RSHIFT LSHIFT AND OR".split()
+    tokens = unparsed.split()
     name = tokens.pop()
     tokens.remove("->")
-    what = ""
-    deps = []
+    what, deps = "", []
     if any(action in tokens for action in acions_with_two_dependents):
         deps.append(tokens[0])
         what = tokens[1]
@@ -60,17 +60,51 @@ def parse_input(input: list[str]):
     return {node.name: node for node in parsed_nodes}
 
 
-def resolve_node(name: str, nodes: dict[str, Node]):
-    node = dict[name]
-    # TODO: implement this
+def resolve_node(find_name: str, nodes: dict[str, Node]):
+
+    cache: dict[str, int] = {}
+
+    def resolve(node_name: str) -> int:
+        if node_name in cache:
+            return cache[node_name]
+
+        if node_name.isdecimal():
+            return int(node_name)
+
+        name, what, deps = nodes[node_name]
+        value = 0
+
+        if what == "RSHIFT":
+            value = resolve(deps[0]) >> resolve(deps[1])
+        elif what == "LSHIFT":
+            value = resolve(deps[0]) << resolve(deps[1])
+        elif what == "AND":
+            value = resolve(deps[0]) & resolve(deps[1])
+        elif what == "OR":
+            value = resolve(deps[0]) | resolve(deps[1])
+        elif what == "NOT":
+            value = ~resolve(deps[0])
+        else:
+            value = resolve(deps[0])
+
+        cache[node_name] = value
+        return value
+
+    return resolve(find_name)
 
 
 def part1(input: list[str]):
     print("\n~~~ Part 1 ~~~\n")
+    nodes = parse_input(input)
+    print(f"resolved a: {resolve_node('a', nodes)}")
 
 
 def part2(input: list[str]):
     print("\n~~~ Part 2 ~~~\n")
+    nodes = parse_input(input)
+    val_a = resolve_node('a', nodes)
+    nodes['b'] = Node(name='b', what="NO-ACTION", deps=[f"{val_a}"])
+    print(f"resolved new a: {resolve_node('a', nodes)}")
 
 
 if __name__ == "__main__":
