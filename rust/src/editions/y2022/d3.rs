c245c9c7ba6f687cb49d5857b5eb82aab9049403
cidit <cidit>
2022-12-03 14:22:20 -0500

    improved 2022/3 rust

--- rust/src/editions/y2022/d3.rs
+++ rust/src/editions/y2022/d3.rs
@@ -1,7 +1,9 @@
 pub fn p1(input: &str) -> String {
     input.lines()
         .map(|s| s.split_at(s.len()/2))
-        .flat_map(|(left, right)| left.chars().find(|&c| right.contains(c)))
+        .flat_map(|(left, right)| {
+            left.chars().find(|&c| right.contains(c))
+        })
         .flat_map(item_code_to_priority)
         .sum::<u32>()
         .to_string()
@@ -11,14 +13,26 @@ pub fn p2(input: &str) -> String {
     input.lines()
         .collect::<Vec<_>>()
         .chunks(3)
-        .flat_map(|elves| elves[0].chars().find(|&c| elves[1].contains(c) && elves[2].contains(c)))
+        .flat_map(|elves| {
+            elves[0]
+                .chars()
+                .find(|&c| {
+                    elves[1].contains(c) && 
+                        elves[2].contains(c)
+                })
+        })
         .flat_map(item_code_to_priority)
         .sum::<u32>()
         .to_string()
 }
 
 fn item_code_to_priority(item_code: char) -> Option<u32> {
-    if !item_code.is_alphabetic() { None }
-    else if item_code.is_lowercase() { Some(item_code as u32 - 97 + 1) }
-    else { Some(item_code as u32 - 65 + 27) }
+    if !item_code.is_alphabetic() { 
+        None 
+    } else if item_code.is_lowercase() { 
+        //    ASCII code         ASCII offset     priority offset
+        Some( item_code as u32   -97              +1             ) 
+    } else { 
+        Some( item_code as u32   -65              +27            ) 
+    }
 }
