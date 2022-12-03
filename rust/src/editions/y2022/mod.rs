159227d7f48101fc1ebfbfa3445e44c48dc65a57
cidit <cidit>
2022-12-03 02:46:30 -0500

    2022/3 works in rust

--- rust/src/editions/y2022/mod.rs
+++ rust/src/editions/y2022/mod.rs
@@ -1,5 +1,6 @@
 mod d1;
 mod d2;
+mod d3;
 use crate::util::model::Solution;
 use std::collections::HashMap;
 
@@ -9,5 +10,7 @@ pub fn solutions() -> HashMap<String, Solution> {
         ("2022/1/2".to_owned(), d1::p2 as Solution),
         ("2022/2/1".to_owned(), d2::p1 as Solution),
         ("2022/2/2".to_owned(), d2::p2 as Solution),
+        ("2022/3/1".to_owned(), d3::p1 as Solution),
+        ("2022/3/2".to_owned(), d3::p2 as Solution),
     ])
 }
